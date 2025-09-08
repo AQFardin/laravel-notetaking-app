a032a53b92eb4d23361dfb2a92b9053d5eabdb77
AQFardin <133776864+AQFardin@users.noreply.github.com>
2025-09-08 09:22:35 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
+// vite.config.js
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'], // <-- CSS entry removed
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
